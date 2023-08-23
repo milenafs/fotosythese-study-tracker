4d685aeac517a11c7326a2dfe7b098eb3e7a4af2
Milena Furuta Shishito <milena.shishito@ifood.com.br>
2023-08-23 09:31:53 -0300

    feat: :sparkles: New function Flower created to make it easier to add a flower on the html making it like a generic component

---
+++ src/flower.ts
@@ -0,0 +1,24 @@
+export default function createFlowers (hours:number, minutes: number, seconds: number, farm: HTMLDivElement) {
+    const Flower = (img:string, txt:string) => {
+        return(`
+            <div class="flower flower-day-mode grow-flower">
+                <h3>${txt}</h3>
+                <img src="${img}"/>
+            </div>
+        `);
+    };
+    
+    const generateRandomFlower = () => {
+        let randomNum = Math.floor(Math.random()*19 + 1);
+        document.querySelector<HTMLDivElement>('.grow-flower')?.classList.remove('grow-flower');
+        farm.innerHTML += Flower(`./assets/images/flowers/${randomNum}.png`,'+ O2');
+    }
+    
+    if(minutes % 15 === 0.0){
+        generateRandomFlower();
+    }
+    if(hours === 0 && minutes === 0 && seconds === 0){
+        farm.innerHTML = '';
+    }
+}
+
