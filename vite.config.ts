f6e2d649c62306354f105699e8edaa2c8fb4c810
Milena Furuta Shishito <milena.shishito@ifood.com.br>
2023-08-24 10:54:20 -0300

    refactor: :lipstick: Changed the style file type from css to scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "vite";
+import sass from 'sass'
 
 export default defineConfig({
     base: "/fotosythese-study-tracker/",
+    css: {
+        preprocessorOptions: {
+            scss: {
+                implementation: sass,
+            },
+        },
+    },
 })
